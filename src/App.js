eb94a843d2c80050684a1ad74b1ebafcb323f4d9
Shrikant Yadav <shrikant9907@gmail.com>
2022-05-21 16:14:29 +0530

    stop watch update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import StopWatch from "./components/StopWatch";
 
 function App() {
   return (
     <React.Fragment>
-      <p>First React App</p>
+      <StopWatch />
     </React.Fragment>
   );
 }
